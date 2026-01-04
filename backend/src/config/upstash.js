216af618cc34197671b9a9be42e078579cfa16ba
Tamanna Rath <tamannaarath@gmail.com>
2026-01-04 12:59:24 +0530

    Update upstash.js

--- backend/src/config/upstash.js
+++ backend/src/config/upstash.js
@@ -1,12 +1,12 @@
-import { Ratelimit } from "@upstash/ratelimit";
-import { Redis } from "@upstash/redis";
-import dotenv from "dotenv";
+//import { Ratelimit } from "@upstash/ratelimit";
+//import { Redis } from "@upstash/redis";
+//import dotenv from "dotenv";
 
-dotenv.config();
+//dotenv.config();
 
-const ratelimit = new Ratelimit({
-  redis: Redis.fromEnv(),
-  limiter: Ratelimit.slidingWindow(100, "60 s"), 
-});
+//const ratelimit = new Ratelimit({
+//  redis: Redis.fromEnv(),
+//  limiter: Ratelimit.slidingWindow(100, "60 s"), 
+//});
 
-export default ratelimit;
+//export default ratelimit;
